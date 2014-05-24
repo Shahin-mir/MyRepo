b4dfa27b30ef5349b3ce8cceee4420e961fd7697
Shahin-mir <Shahin.mirh@gmail.com>
2014-05-24 17:37:50 +1000

    only if version

--- LFacetIndexerAndSearcher/src/main/java/com/application/FacetLuceneSearcher.java
+++ LFacetIndexerAndSearcher/src/main/java/com/application/FacetLuceneSearcher.java
@@ -6,6 +6,7 @@ package com.application;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -57,9 +58,11 @@ public class FacetLuceneSearcher {
 				IndexReader indexReader = DirectoryReader.open(FSDirectory.open(new File(indexDirectory)));
 				IndexSearcher indexSearcher = new IndexSearcher(indexReader);
 				TaxonomyReader taxonomyReader = new DirectoryTaxonomyReader(FSDirectory.open(new File(taxonomyDirectory)));
-//************************************************
-		FacetSearchParams searchParams = new FacetSearchParams(
-				new DefaultFacetIndexingParams());
+//*****************************************************************
+		List<Document> lDoc = new ArrayList<Document>();//list to add doc
+		ScoreDoc tempscoreDoc = null;
+		FacetSearchParams searchParams = new FacetSearchParams(new DefaultFacetIndexingParams());
+		Analyzer analyzer = new MyLowerCaseKeywordAnalyzer();
 		Query luceneQuery = null;
 		System.out.println("Seprate Fields by '/' \n"
 						+ "Identify The Field(s) to specify the search or press Enter to search on all the Fields: ");
@@ -68,7 +71,8 @@ public class FacetLuceneSearcher {
 		System.out.println("Search : ");
 		String fieldtoSearch = scanIn.nextLine();
 		
-		if (identifiedField.length() != 0) {
+		
+		if (identifiedField.toString().length() > 0) {
 
 			QueryPharser queryPharser = new QueryPharser();
 			List<String> map = queryPharser.getQueryMap(identifiedField);
@@ -76,61 +80,40 @@ public class FacetLuceneSearcher {
 			for (int i = 0; i < map.size(); i++) {
 				System.out.println("Searching on Field " + map.get(i));
 				searchParams.addFacetRequest(new CountFacetRequest(new CategoryPath(map.get(i)), 100));
-
-				Analyzer analyzer = new MyLowerCaseKeywordAnalyzer();
-				MultiFieldQueryParser queryParser = new MultiFieldQueryParser(LUCENE_VERSION, new String[] { map.get(i) }, analyzer);
-				luceneQuery = queryParser.parse(fieldtoSearch);
-System.out.println("testing");
-			}// end for
-		} else {
-			System.out.println("Search : ");
-
-			String allFields = prop.getProperty("allFields");
-
-			QueryPharser queryPharser = new QueryPharser();
-			List<String> map = queryPharser.getQueryMap(allFields);
-			System.out.println("Searching on all Fields ");
-
-			for (int i = 0; i < map.size(); i++) {
-				System.out.println("Searching on Field " + map.get(i));
-				searchParams.addFacetRequest(new CountFacetRequest(new CategoryPath(map.get(i)), 100));
-
-				Analyzer analyzer = new MyLowerCaseKeywordAnalyzer();
-				MultiFieldQueryParser queryParser = new MultiFieldQueryParser(LUCENE_VERSION, new String[] { map.get(i) }, analyzer);
-				luceneQuery = queryParser.parse(fieldtoSearch);
-
-			}// end for
-		}//end else	
 				
-				 scanIn.close(); 
+				MultiFieldQueryParser queryParser = new MultiFieldQueryParser(LUCENE_VERSION, new String[] { map.get(i) }, analyzer);
+				luceneQuery = queryParser.parse(fieldtoSearch);
+				scanIn.close(); 
+		
+			
  //*************************************************
         TopScoreDocCollector topScoreDocCollector = TopScoreDocCollector.create(10, true);
 		FacetsCollector facetsCollector = new FacetsCollector(searchParams, indexReader, taxonomyReader);
 		indexSearcher.search(luceneQuery, MultiCollector.wrap(topScoreDocCollector, facetsCollector));//MultiCollector in order to do the search and collect the results and collect the facet
-		
-				/*
-		if(topScoreDocCollector.topDocs()!=null){
-			;*/
-		System.out.println("Found:");
-		for(ScoreDoc scoreDoc: topScoreDocCollector.topDocs().scoreDocs) { 
-			Document document = indexReader.document(scoreDoc.doc);
 			
+		
+		
+		for( ScoreDoc scoreDoc: topScoreDocCollector.topDocs().scoreDocs) { 
+			Document document = indexReader.document(scoreDoc.doc);
+			lDoc.add(document);
+			tempscoreDoc = scoreDoc;
+			
+		        }//end forloop search	
+			}
+			if(lDoc!=null && lDoc.size()>0){
+			System.out.println("Found:");
+			Document document = lDoc.get(lDoc.size()-1);
 			System.out.printf("Concept Object: conceptId=%s, conceptActive=%s, FSN=%s, relativeIdnType=%s, destinationConceptId=%s, relationType=%s,synonymDetails=%s,score=%f\n",
-					document.get("conceptId"), document.get("conceptActive"),
+					document.get("conceptId"), 
+					document.get("conceptActive"),
 					document.get("FSN"),
 					document.get("relativeIdnType"),
 					document.get("destinationConceptId"),
 					document.get("relationType"),
-					document.get("synonymDetails"),
-					scoreDoc.score);
-		
-			
-	
+					document.get("synonymDetails"),tempscoreDoc.score);}else {System.out.println("No results Found !");}
+		  } // end if
 		taxonomyReader.close();
 		indexReader.close();
-		
-		}//end for
-		
 	}//end of main
 		
 }
